0cce93b1d79716782de548f2df1e2f02759765d5
Gabriel Sardinha Ferreira <gabrielsardinhaferreira@gmail.com>
2024-11-06 12:24:40 -0300

    Complementando primeira versao

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"sync"
+	"time"
 
 	"github.com/labstack/gommon/log"
 	"gorm.io/driver/sqlite"
@@ -16,6 +18,44 @@ type MessageUnmarshal struct {
 	DestinationWorker int    `json:"destination_worker"`
 	Interval          int    `json:"interval"`
 	Message           string `json:"message"`
+}
+
+type LastMessage struct {
+	Sender  int
+	Message string
+}
+
+var waitGroup sync.WaitGroup
+
+var lastMessages = [5]LastMessage{}
+
+type MessageOutput struct {
+	worker           int
+	worker_listened  int
+	message          string
+	message_listened string
+}
+
+func PrintMessageOfAWorker(worker int, messages []MessageUnmarshal) {
+	// Espera todas as rotinas iniciarem para iniciar ao mesmo tempo
+	waitGroup.Done()
+	waitGroup.Wait()
+	messageOutput := MessageOutput{}
+	for _, msg := range messages {
+
+		messageOutput.worker = msg.Worker
+		messageOutput.worker_listened = lastMessages[msg.DestinationWorker-1].Sender
+		messageOutput.message = msg.Message
+		messageOutput.message_listened = lastMessages[msg.DestinationWorker-1].Message
+
+		fmt.Printf("worker: %d\nworker_listened: %d\nmessage: \"%s\"\nmessage_listened: \"%s\"\n\n",
+			messageOutput.worker, messageOutput.worker_listened, messageOutput.message, messageOutput.message_listened)
+
+		lastMessages[msg.DestinationWorker-1] = LastMessage{Sender: msg.DestinationWorker, Message: msg.Message}
+
+		// * 30 porque acho que o intervalo entre a inicialização de cada rotina está atrapalhando a ordem de impressão
+		<-time.After(time.Duration(msg.Interval) * time.Millisecond * 30)
+	}
 }
 
 func main() {
@@ -84,18 +124,69 @@ func main() {
 	// 	fmt.Printf("%+v\n", msg)
 	// }
 
-	// Imprimindo os dados no formato final
+	//// Imprimindo os dados no formato final
+	//for _, msg := range parsedMessages {
+	//	fmt.Printf("worker: %d\nDestinationWorker: %d\nmessage: \"%s\"\nmessage_listened: \"%s\"\n\n",
+	//		msg.Worker, msg.DestinationWorker, msg.Message, getMessageListened(msg, parsedMessages))
+	//}
+
+	// Uma lista de mensagens para cada worker
+	worker1Messages := make([]MessageUnmarshal, 0)
+	worker2Messages := make([]MessageUnmarshal, 0)
+	worker3Messages := make([]MessageUnmarshal, 0)
+	worker4Messages := make([]MessageUnmarshal, 0)
+	worker5Messages := make([]MessageUnmarshal, 0)
+
+	// O tempo total de espera
+	amountOfTimeToWait := 0
+	// Separar as mensagens em suas respectivas listas
 	for _, msg := range parsedMessages {
-		fmt.Printf("worker: %d\nDestinationWorker: %d\nmessage: \"%s\"\nmessage_listened: \"%s\"\n\n",
-			msg.Worker, msg.DestinationWorker, msg.Message, getMessageListened(msg, parsedMessages))
+		amountOfTimeToWait += msg.Interval
+		switch msg.Worker {
+		case 1:
+			worker1Messages = append(worker1Messages, msg)
+		case 2:
+			worker2Messages = append(worker2Messages, msg)
+		case 3:
+			worker3Messages = append(worker3Messages, msg)
+		case 4:
+			worker4Messages = append(worker4Messages, msg)
+		case 5:
+			worker5Messages = append(worker5Messages, msg)
+		}
+	}
+
+	// Inicia 6 esperas (5 workers + 1 para sincronizar)
+	waitGroup.Add(6)
+	// Imprimir as mensagens de cada worker
+	go PrintMessageOfAWorker(1, worker1Messages)
+	go PrintMessageOfAWorker(2, worker2Messages)
+	go PrintMessageOfAWorker(3, worker3Messages)
+	go PrintMessageOfAWorker(4, worker4Messages)
+	go PrintMessageOfAWorker(5, worker5Messages)
+
+	// Esperar 1 segundo para sincronizar
+	<-time.After(time.Second)
+	// Sincronizar as rotinas
+	waitGroup.Done()
+
+	// c é um canal que recebe sinais do sistema operacional como o interrupt
+	var c chan os.Signal
+
+	// Como a soma de todos os tempos está maior que os tempos paralelos, é melhor lidar com o interrupt
+	select {
+	case <-time.After(time.Duration(amountOfTimeToWait) * time.Millisecond):
+		return
+	case <-c:
+		return
 	}
 }
 
-func getMessageListened(msg MessageUnmarshal, messages []MessageUnmarshal) string {
-	for _, m := range messages {
-		if m.Worker == msg.DestinationWorker {
-			return m.Message
-		}
-	}
-	return "N/A" // Caso não seja encontrada, retorna "N/A"
-}
+//func getMessageListened(msg MessageUnmarshal, messages []MessageUnmarshal) string {
+//	for _, m := range messages {
+//		if m.Worker == msg.DestinationWorker {
+//			return m.Message
+//		}
+//	}
+//	return "N/A" // Caso não seja encontrada, retorna "N/A"
+//}
